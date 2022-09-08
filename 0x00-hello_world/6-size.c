bb0aab575cc86da02da5dd47913c1766b36b102b
Ebenizer Makuo <smartjay034@gmail.com>
2022-09-08 15:16:07 -0700

    Modified the file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a c is: %lu.\n", (unsigned long)sizeof(c));
+	return (0);
+}
