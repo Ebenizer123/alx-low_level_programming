682c7944df3d35a468d6d674bbf0803e4cd8a38a
Ebenizer Makuo <smartjay034@gmail.com>
2022-09-13 22:22:40 +0100

    modified file 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main -check code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return: 0
  */
 void print_alphabet_x10(void)
 {
