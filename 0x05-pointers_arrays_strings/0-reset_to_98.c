0f4efa0827f1796421fc71c4c9c4931e8d0a668b
Ebenizer Makuo <smartjay034@gmail.com>
2022-09-20 02:41:39 +0100

    Added new files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a function that takes a pointer to an int
+ *
+ * @n: takes an int
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
